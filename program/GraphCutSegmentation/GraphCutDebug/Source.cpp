4a9d52aa89c03a5aaff2a0920de8a9a85a2081f7
uGPin12 <deeplydrinking@gmail.com>
2020-05-07 22:21:38 +0900

    add comment

--- program/GraphCutSegmentation/GraphCutDebug/Source.cpp
+++ program/GraphCutSegmentation/GraphCutDebug/Source.cpp
@@ -11,11 +11,11 @@ double calc_likelihood(double value, double ave, double var) {
 }
 
 int main() {
-
-	std::vector<input_type>	label	= { 100,90,90,20,20,10 };
-	std::vector<seed_type>		seed	= { 1,1,0,0,2,2 };
-
-	std::vector<mask_type> mask(label.size(), 1);
+	
+	// make image
+	std::vector<input_type>	label	= { 100, 90, 90, 20, 20, 10 };
+	std::vector<seed_type>	seed	= {	  1,  1,  0,  0,  2,  2 };
+	std::vector<mask_type>	mask(label.size(), 1);
 
 	mkdir_boost("../../../../result/debug/");
 	//save_vector("../../../../result/d/*ebug/1D_label.mhd"	, label	, { 6 }, { 1 }, true);
@@ -26,6 +26,7 @@ int main() {
 	std::cout << "Segmentation starts : lambda = " << lambda << ", sigma = " << sigma << std::endl;
 	std::cout << std::endl;
 
+	// Compute average and variance of seed's value
 	double ave_obj = 0, ave_bkg = 0, var_obj = 0, var_bkg = 0;
 	unsigned int obj_seed = 0, bkg_seed = 0;
 	for (unsigned int s = 0; s < 6; s++) {
@@ -56,17 +57,21 @@ int main() {
 	std::cout << "Background	: " << ave_bkg << "(+- " << std::sqrt(var_bkg) << " )" << std::endl;
 	std::cout << std::endl;
 
-	std::vector<feat_type> imgObj, imgBkg;
+	// Conpute weight of t-link(ignore seed)
+	std::vector<feat_type> imgObj(6, 0), imgBkg(6, 0);
 	for (unsigned int s = 0; s < 6; s++) {
-		imgObj.push_back(calc_likelihood(static_cast<feat_type>(label[s]), ave_obj, var_obj));
-		imgBkg.push_back(calc_likelihood(static_cast<feat_type>(label[s]), ave_bkg, var_bkg));
-		std::cout << "val = " << label[s] << ", obj = " << imgObj[s] << " , bkg = " << imgBkg[s] << std::endl;
+		if (seed[s] == 0) {
+			imgObj[s] = calc_likelihood(static_cast<feat_type>(label[s]), ave_obj, var_obj);
+			imgBkg[s] = calc_likelihood(static_cast<feat_type>(label[s]), ave_bkg, var_bkg);
+			std::cout << "val = " << label[s] << ", obj = " << imgObj[s] << " , bkg = " << imgBkg[s] << std::endl;
+		}
 	}
 
 	std::vector<output_type> imgO(label.size(), 0), labelO(label.size(), 0);
-	graph_cut_segmentation_6n(imgO, labelO, label, mask, imgObj, imgBkg, { 6,1,1 }, lambda, sigma);;
+	graph_cut_segmentation(imgO, labelO, label, mask, seed, imgObj, imgBkg, { 6,1,1 }, lambda, sigma);
 
-	//save_vector("../../../../result/debug/1D_result.mhd", imgO, { 6 }, { 1 }, true);
+	// Save result
+	save_vector("../../../../result/debug/1D_result.mhd", imgO, { 6 }, { 1 }, true);
 
 	system("pause");
 	return 0;
